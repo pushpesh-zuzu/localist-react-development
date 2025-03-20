e4eb5c9ba3b90d2f05ee172e144b91f439db7ec5
Bhavya <joshibhavya64@gmail.com>
2025-03-20 16:53:53 +0530

    refactor: enhance responsive design and layout adjustments across service panels

--- src/component/servicePanel/FindLocalJobs/FindLocalJobs.jsx
+++ src/component/servicePanel/FindLocalJobs/FindLocalJobs.jsx
@@ -2,48 +2,50 @@ import React, { useEffect, useState } from "react";
 import styles from "./FindLocalJobs.module.css";
 import { PopularServiceData } from "../../../constant/ServicePanel";
 import { useDispatch, useSelector } from "react-redux";
-import { getPopularServiceList, searchService, setService, } from "../../../store/FindJobs/findJobSlice";
+import {
+  getPopularServiceList,
+  searchService,
+  setService,
+} from "../../../store/FindJobs/findJobSlice";
 import { useNavigate } from "react-router-dom";
-import { LoadingOutlined } from '@ant-design/icons';
+import { LoadingOutlined } from "@ant-design/icons";
 import { generateSlug } from "../../../utils";
 import { Spin } from "antd";
 
 const FindLocalJobs = () => {
-    const [Input,setInput] = useState("")
-    const [selectedService, setSelectedService] = useState(null);
-    const dispatch = useDispatch()
-    const {popularList,service,popularLoader,searchServiceLoader} = useSelector((state) => state.findJobs)
-    const navigate = useNavigate();
+  const [Input, setInput] = useState("");
+  const [selectedService, setSelectedService] = useState(null);
+  const dispatch = useDispatch();
+  const { popularList, service, popularLoader, searchServiceLoader } =
+    useSelector((state) => state.findJobs);
+  const navigate = useNavigate();
   const handleServiceClick = (service) => {
     const slug = generateSlug(service.banner_title);
     navigate(`/sellers/create-account/${slug}`);
   };
-    useEffect(()=>{
-dispatch(getPopularServiceList())
-return(()=> 
-  dispatch(setService([]))
-)
-    },[])
-    useEffect(() => {
-        const delayDebounce = setTimeout(() => {
-            if (Input.trim() !== "") {
-                dispatch(searchService({ search: Input }));
-                
-            }
-        }, 500);
+  useEffect(() => {
+    dispatch(getPopularServiceList());
+    return () => dispatch(setService([]));
+  }, []);
+  useEffect(() => {
+    const delayDebounce = setTimeout(() => {
+      if (Input.trim() !== "") {
+        dispatch(searchService({ search: Input }));
+      }
+    }, 500);
 
-        return () => clearTimeout(delayDebounce);
-    }, [Input, dispatch]);
-    const handleSelectService = (item) => {
-        setInput(item.banner_title); 
-        setSelectedService(item);
-    };
-    const handleGetStarted = () => {
-        if (selectedService) {
-            const slug = generateSlug(selectedService.banner_title);
-            navigate(`/sellers/create-account/${slug}`);
-        }
-    };
+    return () => clearTimeout(delayDebounce);
+  }, [Input, dispatch]);
+  const handleSelectService = (item) => {
+    setInput(item.banner_title);
+    setSelectedService(item);
+  };
+  const handleGetStarted = () => {
+    if (selectedService) {
+      const slug = generateSlug(selectedService.banner_title);
+      navigate(`/sellers/create-account/${slug}`);
+    }
+  };
   return (
     <div className={styles.container}>
       {/* Left Section */}
@@ -61,49 +63,62 @@ return(()=>
             className={styles.searchInput}
             placeholder="What service do you provide?"
             onChange={(e) => {
-                setInput(e.target.value);
-                if (!e.target.value) {
-                    dispatch(setService([]))
-                }
-                setSelectedService(null); 
+              setInput(e.target.value);
+              if (!e.target.value) {
+                dispatch(setService([]));
+              }
+              setSelectedService(null);
             }}
             value={Input}
           />
- 
-        {service?.length > 0 && (
-                        <div className={styles.searchResults}>
-                            {searchServiceLoader ? <Spin indicator={<LoadingOutlined spin  />}/> : <> {service.map((item) => (
-                                <p 
-                                    key={item.id} 
-                                    className={styles.searchItem} 
-                                    onClick={() => handleSelectService(item)}
-                                >
-                                    {item.banner_title}
-                                </p>
-                            ))}</>}
-                           
-                        </div>
-                    )}
-                    
-                
 
-          <button  onClick={handleGetStarted}>Get started</button>
+          {service?.length > 0 && (
+            <div className={styles.searchResults}>
+              {searchServiceLoader ? (
+                <Spin indicator={<LoadingOutlined spin />} />
+              ) : (
+                <>
+                  {" "}
+                  {service.map((item) => (
+                    <p
+                      key={item.id}
+                      className={styles.searchItem}
+                      onClick={() => handleSelectService(item)}
+                    >
+                      {item.banner_title}
+                    </p>
+                  ))}
+                </>
+              )}
+            </div>
+          )}
+
+          <button onClick={handleGetStarted}>Get started</button>
         </div>
       </div>
 
       {/* Right Section */}
       <div className={styles.rightSection}>
         <h3>Popular services</h3>
-        {popularLoader ? <Spin indicator={<LoadingOutlined spin  style={{color:"primary"}}/>} className={styles?.loaderDesign}/> : 
-        <div className={styles.servicesList}>
-          {popularList.map((service, index) => (
-            <div key={service.id} className={styles.serviceItem}  onClick={() => handleServiceClick(service)}>
-              <img src={service.banner_image} alt={service.title} />
-              <span>{service.banner_title}</span>
-            </div>
-          ))}
-        </div>
-}
+        {popularLoader ? (
+          <Spin
+            indicator={<LoadingOutlined spin style={{ color: "primary" }} />}
+            className={styles?.loaderDesign}
+          />
+        ) : (
+          <div className={styles.servicesList}>
+            {popularList.map((service, index) => (
+              <div
+                key={service.id}
+                className={styles.serviceItem}
+                onClick={() => handleServiceClick(service)}
+              >
+                <img src={service.banner_image} alt={service.title} />
+                <span>{service.banner_title}</span>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
