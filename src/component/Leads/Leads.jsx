6e49bd498be5d31f2685604adebc4e8412a8b72b
Bhavya <joshibhavya64@gmail.com>
2025-04-04 11:39:26 +0530

    lead setting ui

--- src/component/Leads/Leads.jsx
+++ src/component/Leads/Leads.jsx
@@ -3,12 +3,14 @@ import styles from "./Leads.module.css";
 import FeelingStuck from "./FeelingStuck/FeelingStuck";
 import MatchingLeads from "./MatchingLeads/MatchingLeads";
 import LeadSettings from "./LeadSettings/LeadSettings";
+import CustomerQuestions from "./LeadSettings/CustomerQuestions";
 
 const Leads = () => {
   return (
     <>
       <div className={styles.leadsOverlay}>
         <LeadSettings />
+        <CustomerQuestions />
         {/* <FeelingStuck />
         <MatchingLeads /> */}
       </div>
