c435e3a36585936e182af282b0bce07bd425adad
Bhavya <joshibhavya64@gmail.com>
2025-04-16 10:17:56 +0530

    pricing ui design

---
+++ src/component/Pricing/PricingCards/PricingCards.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./PricingCards.module.css";
+
+const PricingCards = () => {
+  return <div>PricingCards</div>;
+};
+
+export default PricingCards;
---
+++ src/component/Pricing/StartWinning/StartWinning.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./StartWinning.module.css";
+
+const StartWinning = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.heading}>Start winning new business today</p>
+      <button className={styles.button}>Join as a Professional</button>
+    </div>
+  );
+};
+
+export default StartWinning;
