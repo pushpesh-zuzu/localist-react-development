9228af6f3bfa7c2f28c10ee11e3003cf9504f84a
Ajayzuzu <ajay@zuzucodes.com>
2025-06-12 17:12:20 +0530

    view profile improvement

--- src/component/ViewProfile/Photos/Photos.jsx
+++ src/component/ViewProfile/Photos/Photos.jsx
@@ -1,13 +1,18 @@
 import React from "react"
 import styles from "./Photos.module.css"
 import DummyImage from "../../../assets/Images/DummyImage.svg";
-
+import paginationImg from "../../../assets/Icons/MyResponse/paginationImg.svg"
+import leftpaginationImg from "../../../assets/Icons/MyResponse/rightPagenationImg.svg"
 const Photos = () => {
   return (
     <div className={styles.photoContainer} >
       <h2>Photos</h2>
       <div className={styles.photosContainer}>
         <img src={DummyImage} alt="Profile" className={styles.profileImg} />
+      </div>
+      <div className={styles.paginationBox}>
+        <img src={leftpaginationImg} alt="image" />
+        <img src={paginationImg} alt="image" />
       </div>
     </div>
   )
