41afbbe4cd5ac37511e8ded23335f107d92ae5cc
athirababu <athirakottara@gmail.com>
2025-07-16 14:06:35 +0530

    updation in reload

--- src/component/Leads/Leads.jsx
+++ src/component/Leads/Leads.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import { useDispatch } from "react-redux";
 import styles from "./Leads.module.css";
 import LeadSettings from "./LeadSettings/LeadSettings";
 import CustomerQuestions from "./LeadSettings/CustomerQuestions";
 import LeadLists from "./LeadLists/LeadLists";
+import { setRegisterStep, clearServiceFormData } from "../../store/FindJobs/findJobSlice";
 
 const Leads = () => {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setRegisterStep(0)); 
+    dispatch(clearServiceFormData());
+  }, []);
+
   // const [selectedService, setSelectedService] = useState(null);
-  // const [LeadOpen,setLeadOpen] = useState(false)
+  // const [LeadOpen,setLeadOpen] = useState(false)yy
   return (
     <>
       <div className={styles.leadsOverlay}>
