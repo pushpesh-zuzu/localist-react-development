ed905adb68b4c5ce5f42bd36d1f43aa4c8b9d9bf
athirababu <athirakottara@gmail.com>
2025-07-16 14:23:10 +0530

    updation revert

--- src/component/Leads/Leads.jsx
+++ src/component/Leads/Leads.jsx
@@ -1,19 +1,11 @@
-import React, { useState,useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from "react";
 import styles from "./Leads.module.css";
 import LeadSettings from "./LeadSettings/LeadSettings";
 import CustomerQuestions from "./LeadSettings/CustomerQuestions";
 import LeadLists from "./LeadLists/LeadLists";
-import { setRegisterStep, clearServiceFormData } from "../../store/FindJobs/findJobSlice";
+
 
 const Leads = () => {
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(setRegisterStep(0)); 
-    dispatch(clearServiceFormData());
-  }, []);
-
   // const [selectedService, setSelectedService] = useState(null);
   // const [LeadOpen,setLeadOpen] = useState(false)yy
   return (
