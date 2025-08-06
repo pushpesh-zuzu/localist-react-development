00fe732c794c8e08f4a36474fbd240995cec2547
Deepak Birla <deepak@zuzucodes.com>
2025-08-06 22:36:32 +0530

    fix-some-performance-issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-  }
-})
+    jsxFactory: "h",
+    jsxFragment: "Fragment",
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+    },
+  },
+  server: {
+    headers: {
+      "Cache-Control": "public, max-age=3600",
+    },
+  },
+});
