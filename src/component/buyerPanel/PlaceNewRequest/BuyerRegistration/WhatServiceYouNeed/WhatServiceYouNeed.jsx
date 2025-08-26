9b41a11d5084f4b30bb75524ff1b981ed2672003
athirababu <athirakottara@gmail.com>
2025-08-26 10:01:14 +0530

    updation in magic link

--- src/component/buyerPanel/PlaceNewRequest/BuyerRegistration/WhatServiceYouNeed/WhatServiceYouNeed.jsx
+++ src/component/buyerPanel/PlaceNewRequest/BuyerRegistration/WhatServiceYouNeed/WhatServiceYouNeed.jsx
@@ -138,7 +138,7 @@ const handleContinue = useCallback(() => {
   if (!pincode) {
     newErrors.pincode = "Postcode is required!";
   } else if (pincode.length < 5 || pincode.length > 8) {
-    newErrors.pincode = "Postcode must be exactly 5 or 8 characters!";
+    newErrors.pincode = "Postcode must be between  5 or 8 characters!";
   }
 
   // if (!isPincodeFromDropdown) {
