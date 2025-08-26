a552e546bdd2425c5eb616b0c5e8deed881a64cf
Deepak Birla <deepak@zuzucodes.com>
2025-08-26 15:21:09 +0530

    revert-changes

--- src/component/MyProfile/MyProfile.jsx
+++ src/component/MyProfile/MyProfile.jsx
@@ -149,8 +149,8 @@ const randomKey = user_id; //Math.random().toString(36).substring(2, 8);
      {companySlug && (
         <a
           className={styles.profileLink}
-          // href={`/view-profile/${companySlug.toLowerCase()}/${randomKey}`}
-          href={`/review/${companySlug.toLowerCase()}/${userToken.uuid}`}
+          href={`/view-profile/${companySlug.toLowerCase()}/${randomKey}`}
+          // href={`/review/${companySlug.toLowerCase()}/${userToken.uuid}`}
           target="_blank"
           rel="noopener noreferrer"
         >
