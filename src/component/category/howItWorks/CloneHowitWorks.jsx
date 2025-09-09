ceb057a2b8de9981609a397cf6d7a446b7eed6bd
Deepak Birla <deepak@zuzucodes.com>
2025-09-09 11:21:22 +0530

    fix-heading-css-home-garden

--- src/component/category/howItWorks/CloneHowitWorks.jsx
+++ src/component/category/howItWorks/CloneHowitWorks.jsx
@@ -5,7 +5,7 @@ const HowItWorks = ({ctaText,howItWorksData}) => {
       <div className={styles.container}>
         <h2 className={styles.heading}>
         How  
-        <span className={styles.highlight}> Localists Works</span>
+        <span className={styles.highlight}> Localists </span>Works
         </h2>
         <div className={styles.stepsContainer}>
           {howItWorksData.map((item, index) => (
