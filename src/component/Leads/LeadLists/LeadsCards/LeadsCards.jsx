df44bba6fb04ca0f17afffb2eac037ebad60cb8f
Ajayzuzu <ajay@zuzucodes.com>
2025-09-15 10:16:49 +0530

    save for later issue fixed

--- src/component/Leads/LeadLists/LeadsCards/LeadsCards.jsx
+++ src/component/Leads/LeadLists/LeadsCards/LeadsCards.jsx
@@ -592,6 +592,7 @@ const LeadsCards = ({ enoughCredit }) => {
                             <button
                               className={styles.saveBtn}
                               onClick={() => handleSaveLater(item)}
+                              disabled={saveLaterLoaderId === item.id}
                             >
                               {saveLaterLoaderId === item.id ? (
                                 <Spin
