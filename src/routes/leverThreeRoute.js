3da556b8fffc8da21dd949ce1547f536329c63ad
Deepak Birla <deepak@zuzucodes.com>
2025-09-22 21:27:57 +0530

    remove-tutor-page

--- src/routes/leverThreeRoute.js
+++ src/routes/leverThreeRoute.js
@@ -13,6 +13,6 @@ const levelThreePagesRoutes = [
   { path: "airport-transfers-near-me", Component: AirportServices },
   { path: "gutter-cleaning-near-me", Component: GutterCleaner },
   { path: "tree-surgeon-near-me", Component: TreeSurgeon },
-  { path: "tutors-near-me", Component: Tutor },
+  // { path: "tutors-near-me", Component: Tutor },
 ];
 export { levelThreePagesRoutes };
