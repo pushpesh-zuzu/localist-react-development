9e8409370ccf6477084e354afff69cf3615f061e
Deepak Birla <deepak@zuzucodes.com>
2025-10-09 17:42:47 +0530

    uncomment-close-button

--- src/component/buyerPanel/PlaceNewRequest/BuyerRegistration/EmailMatch/EmailMatch.jsx
+++ src/component/buyerPanel/PlaceNewRequest/BuyerRegistration/EmailMatch/EmailMatch.jsx
@@ -206,7 +206,7 @@ const EmailMatch = ({
   return (
     <div className={styles.modalOverlay}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-        {/* {!hideCloseButton && (
+        {!hideCloseButton && (
           <button
             className={styles.closeButton}
             onClick={handleCloseClick}
@@ -214,7 +214,7 @@ const EmailMatch = ({
           >
             &times;
           </button>
-        )} */}
+        )}
         <div className={styles.header}>
           <h2>YOU ARE ONLY ONE STEP FROM COMPARING FREE QUOTES!</h2>
           <p style={{ color: "#000" }}>
