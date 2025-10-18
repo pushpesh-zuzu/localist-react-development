ee01e891e76053ecc04d85943a2f8af0523d107d
Deepak Birla <deepak@zuzucodes.com>
2025-10-18 20:49:10 +0530

    revert-handlescrolltoBottom

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -1,14 +1,3 @@
 export const handleScrollToBottom = () => {
-  // Method 1: Regular scroll
   window.scrollTo({ top: 0, behavior: "smooth" });
-  
-  // Method 2: Fallback for iOS
-  setTimeout(() => {
-    document.body.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }, 100);
-  
-  // Method 3: Alternative for Safari
-  setTimeout(() => {
-    document.documentElement.scrollTop = 0;
-  }, 200);
 };
