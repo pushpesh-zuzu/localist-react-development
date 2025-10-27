a7838cf781aba008da883cd47ee468a9ae86fd60
Ajayzuzu <ajay@zuzucodes.com>
2025-10-27 13:53:37 +0530

    add multiple credit card, remove and primary

--- src/component/MyCredit/MyCredit/CreditCard.jsx
+++ src/component/MyCredit/MyCredit/CreditCard.jsx
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from "react";
 import styles from "./CreditCard.module.css";
 import visaImg from "../../../assets/Images/Setting/Visa.svg";
 import { useDispatch, useSelector } from "react-redux";
-import { getSellerCardApi } from "../../../store/MyProfile/MyCredit/MyCreditSlice";
+import {
+  getSellerCardApi,
+  removeCardDetailsApi,
+  makePrimaryApi,
+} from "../../../store/MyProfile/MyCredit/MyCreditSlice";
 import AddCardModal from "../MyPaymentDetails/AddCardModal";
 
 const CreditCard = () => {
   const [isopen, setIsOpen] = useState(false);
+  const [primaryId, setPrimaryId] = useState(0);
   const dispatch = useDispatch();
   const { getSellerCardData } = useSelector((state) => state.myCredit);
   const cardNumber = getSellerCardData?.map((item) => item?.card_number);
@@ -14,47 +19,101 @@ const CreditCard = () => {
   useEffect(() => {
     dispatch(getSellerCardApi());
   }, []);
-  const handleChangeModal = () => {
+
+  const handleAddCard = () => {
     setIsOpen(true);
   };
+
+  const handleRemoveCard = (data) => {
+    dispatch(removeCardDetailsApi({ card_id: data?.id }));
+    dispatch(getSellerCardApi());
+  };
+
+  const handlePrimaryChange = (data) => {
+    dispatch(makePrimaryApi({ card_id: data?.id, user_id: data?.user_id }));
+  };
+
   return (
     <>
-      <div className={styles.container}>
-        {getSellerCardData?.length ? (
-          <>
-            {" "}
-            <div className={styles.visaCard}>
-              <img src={visaImg} alt="Visa" />
+      <div
+        className={
+          !getSellerCardData || getSellerCardData.length < 1
+            ? styles.parent_container
+            : styles.container_wrapper
+        }
+      >
+        {getSellerCardData && getSellerCardData.length > 0 ? (
+          getSellerCardData.map((item, index) => (
+            <div className={styles.container}>
+              <div className={styles.visaCard_wrapper} key={index}>
+                <div className={styles.visaCard}>
+                  <img src={visaImg} alt="Visa" />
+                  <div>
+                    We'll charge the card ending *
+                    {String(item.card_number)?.slice(-4)} that we have on file
+                  </div>
+                  {primaryId == index && <span>Primary</span>}
+                </div>
 
-              <div>
-                We'll charge The Card Ending *{cardNumber?.[0]?.slice(-4)} That
-                We Have On File
+                {/* Uncomment if needed */}
+
+                <div className={styles.remove_actionButtons}>
+                  <span
+                    onClick={() => {
+                      handleRemoveCard(item);
+                    }}
+                    className={styles.rightText}
+                  >
+                    Remove
+                  </span>
+                  {primaryId != index && (
+                    <>
+                      <span className={styles.separator}>|</span>
+                      <span
+                        onClick={() => {
+                          setPrimaryId(index);
+                          handlePrimaryChange(item);
+                        }}
+                        className={styles.rightText}
+                      >
+                        Make Primary
+                      </span>
+                    </>
+                  )}
+                </div>
               </div>
             </div>
-            <div className={styles.rightText} onClick={handleChangeModal}>
-              {" "}
-              Change
-            </div>
-          </>
+          ))
         ) : (
           <>
             <div className={styles.visaCard}>
               <img src={visaImg} alt="Visa" />
-
               <div className={styles.textRight}>
                 Buy more credits and get a bigger discount
               </div>
             </div>
-            <div className={styles.rightText} onClick={handleChangeModal}>
-              {" "}
-              Add
-              {/* | Change | Remove */}
+
+            <div className={styles.actionButtons}>
+              <span onClick={handleAddCard} className={styles.actionText}>
+                Add
+              </span>
             </div>
           </>
         )}
       </div>
+      <div className={styles.actionButtons}>
+        <span onClick={handleAddCard} className={styles.rightText}>
+          Add
+        </span>
+        <span className={styles.separator}>|</span>
+        <span onClick={handleRemoveCard} className={styles.rightText}>
+          Remove
+        </span>
+      </div>
+
       {isopen && <AddCardModal onClose={() => setIsOpen(false)} />}
     </>
   );
 };
+
 export default CreditCard;
