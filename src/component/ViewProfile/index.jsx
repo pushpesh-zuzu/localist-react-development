66cd093f973eac0a0af6c3b22df02dae3233dabf
Ajayzuzu <ajay@zuzucodes.com>
2025-10-28 16:15:24 +0530

    changes

--- src/component/ViewProfile/index.jsx
+++ src/component/ViewProfile/index.jsx
@@ -136,7 +136,7 @@ const ViewProfiles = () => {
       const container = rightContainerRef.current;
       const targetPosition = targetRef.current.offsetTop;
 
-      const OFFSET = 80;
+      const OFFSET = 140;
 
       container.scrollTo({
         top: targetPosition - OFFSET,
