9fdcda34801544e3a603e2e6c65865ef62f5f85b
Deepak Birla <deepak@zuzucodes.com>
2025-10-30 12:40:34 +0530

    add-new-params-lead-buyer-registration

--- src/component/servicePanel/FindLocalJobs/ServiceCreateAccount/OtherServiceStep/OtherServiceStep.jsx
+++ src/component/servicePanel/FindLocalJobs/ServiceCreateAccount/OtherServiceStep/OtherServiceStep.jsx
@@ -12,11 +12,12 @@ import {
 } from "../../../../../store/FindJobs/findJobSlice";
 import { Spin } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { showToast } from "../../../../../utils";
 import LocationIcon from "../../../../../assets/Images/HowItWorks/locationImg.svg";
 import { clearCompanyData } from "../../../../../store/Company/companyLookup";
 import { getCityName } from "../../../../../store/Buyer/BuyerSlice";
+import { extractAllParams } from "../../../../../utils/decodeURLParams";
 
 const OtherServiceStep = ({
   prevStep,
@@ -26,7 +27,12 @@ const OtherServiceStep = ({
 }) => {
   const [isPostcodeFromSuggestion, setIsPostcodeFromSuggestion] =
     useState(false);
-
+    const { search } = useLocation();
+    const allParams = extractAllParams(search || window.location.search);
+    // Convert array to a comma-separated string
+    const utm_campaign = allParams.utm_campaign || "";
+    const utm_source = allParams.utm_source || "";
+    const utm_medium = allParams.utm_medium || "";
   const [Input, setInput] = useState("");
   const [isFocused, setIsFocused] = useState(false);
   const [selectedService, setSelectedService] = useState(null);
@@ -300,6 +306,9 @@ const OtherServiceStep = ({
       expanded_radius:
         selectedServices.length > 0 ? formData.expanded_radius : "",
     };
+    if (utm_source) payload.utm_source = utm_source;
+    if (utm_medium) payload.utm_medium = utm_medium;
+    if (utm_campaign) payload.utm_campaign = utm_campaign;
     payload.coordinates = JSON.stringify(payload.coordinates);
     delete payload.password;
     delete payload.suite;
