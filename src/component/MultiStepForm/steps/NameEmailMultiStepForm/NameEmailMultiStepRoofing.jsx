d434ad9a6b0eee6c3f08f7761eef71c91e23b0ca
Deepak Birla <deepak@zuzucodes.com>
2025-11-10 20:26:08 +0530

    remove-coma

--- src/component/MultiStepForm/steps/NameEmailMultiStepForm/NameEmailMultiStepRoofing.jsx
+++ src/component/MultiStepForm/steps/NameEmailMultiStepForm/NameEmailMultiStepRoofing.jsx
@@ -105,7 +105,7 @@ const NameEmailMultiStepRoofing = ({
               Perfect! We’ve found you some great local matches
             </h3>
             <p className={styles.bannerPara}>
-              One more thing, we need your details just to send you quotes only”
+              One more thing, we need your details just to send you quotes only
             </p>
           </div>
         ) : (
