6643a8489fcb52603af34fbf70662b603fd9a001
Deepak Birla <deepak@zuzucodes.com>
2025-11-11 13:25:00 +0530

    add-location-in-fencing-page-near-me

--- src/component/MultiStepForm/steps/NameEmailMultiStepForm/NameEmailMultiStepForm.jsx
+++ src/component/MultiStepForm/steps/NameEmailMultiStepForm/NameEmailMultiStepForm.jsx
@@ -4,23 +4,19 @@ import styles from "./NameEmailMultiStepForm.module.css";
 import { checkEmailIdApi } from "../../../../store/FindJobs/findJobSlice";
 import { setbuyerRequestData } from "../../../../store/Buyer/BuyerSlice";
 import CardLayoutWrapper from "../CardLayoutWrapper/CardLayoutWrapper";
+import BackgroundWrapperNameEmailMultiForm from "../../BackgroundWrapperNameEmailMultiForm/BackgroundWrapperNameEmailMultiForm";
+import LoaderWithTextMultiStepForm from "../../LoaderWithTextMultiStepForm/LoaderWithTextMultiStepForm";
+import nameEmailBanner from "../nameEmailBanner.webp";
+import CheckStartCircle from "../../../../assets/Icons/CheckStartCircle.png";
 
-const NameEmailMultiStepForm = ({
-  nextStep,
-  setEmails,
-  resetTrigger,
-  isPPCPages = false,
-  onBack,
-}) => {
+const NameEmailMultiStepForm = ({ nextStep, isPPCPages = false, onBack }) => {
   const dispatch = useDispatch();
-  const { errorMessage, searchServiceLoader } = useSelector(
-    (state) => state.findJobs
-  );
+  const { searchServiceLoader } = useSelector((state) => state.findJobs);
   const { buyerRequest } = useSelector((state) => state.buyer);
   const [email, setEmail] = useState(buyerRequest?.email);
   const [name, setName] = useState(buyerRequest?.name);
-
-  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isInitialLoading, setIsInitialLoading] = useState(true);
+  const [isBannerText, setIsBannerText] = useState(false);
   const [emailErrorMessage, setEmailErrorMessage] = useState("");
 
   const [errors, setErrors] = useState({
@@ -53,11 +49,7 @@ const NameEmailMultiStepForm = ({
     setErrors(newErrors);
 
     const hasError = Object.values(newErrors).some((e) => e);
-    if (hasError || (!isPPCPages && !isEmailValid)) return;
-
-    if (!isPPCPages && setEmails) {
-      setEmails(email);
-    }
+    if (hasError) return;
 
     const finalEmail = isPPCPages ? buyerRequest?.email || "" : email;
     const res = await dispatch(checkEmailIdApi({ email }));
@@ -69,78 +61,93 @@ const NameEmailMultiStepForm = ({
     }
   };
 
-  const [showError, setShowError] = useState(false);
-
-  useEffect(() => {
-    if (errorMessage) {
-      setShowError(true);
-      const timer = setTimeout(() => {
-        setShowError(false);
-      }, 9000);
-      return () => clearTimeout(timer);
-    }
-  }, [errorMessage]);
-
-  useEffect(() => {
-    if (resetTrigger) {
-      setName("");
-      setEmail("");
-      setErrors(null);
-    }
-  }, [resetTrigger]);
-
   const handleBackClick = () => {
     onBack();
   };
-
+  const handleBannerText = () => {
+    setIsBannerText(false);
+  };
   return (
-    <CardLayoutWrapper
-      title="You're nearly done! Just enter a few details to get your custom quotes."
-      onButtonClick={handleSubmit}
-      onBackClick={handleBackClick}
-      buttonText="Next"
-      showBackButton={true}
-      disableNextButton={searchServiceLoader}
-      loader={searchServiceLoader}
-    >
-      <div className={styles.infoWrapper}>
-        {!isPPCPages && (
-          <div style={{ marginBottom: "10px" }}>
-            <input
-              type="email"
-              placeholder="Email"
-              className={`${styles.input} ${
-                errors?.email ? styles.inputError : ""
-              }`}
-              value={email}
-              onChange={handleEmailChange}
+    <BackgroundWrapperNameEmailMultiForm backgroundImage={nameEmailBanner}>
+      <CardLayoutWrapper
+        title={
+          isBannerText
+            ? ""
+            : `You're nearly done! Just enter a few details to get your custom quotes.`
+        }
+        onButtonClick={isBannerText ? handleBannerText : handleSubmit}
+        onBackClick={handleBackClick}
+        buttonText="Next"
+        showBackButton={true}
+        disableNextButton={searchServiceLoader || isInitialLoading}
+        loader={searchServiceLoader}
+        NameEmailContainer={true}
+      >
+        {isInitialLoading ? (
+          <LoaderWithTextMultiStepForm
+            setIsInitialLoading={setIsInitialLoading}
+            setIsBannerText={setIsBannerText}
+          />
+        ) : isBannerText && !isInitialLoading ? (
+          <div className={styles.bannerContainer}>
+            <img
+              className={styles.bannerImage}
+              src={CheckStartCircle}
+              alt="CheckIcon"
             />
-            {errors?.email && (
+            <h3 className={styles.bannerHeading}>
+              Perfect! We’ve found you some great local matches
+            </h3>
+            <p className={styles.bannerPara}>
+              One more thing, we need your details just to send you quotes only
+            </p>
+          </div>
+        ) : (
+          <div className={styles.infoWrapper}>
+            {!isPPCPages && (
+              <div style={{ marginBottom: "10px" }}>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className={`${styles.input} ${
+                    errors?.email ? styles.inputError : ""
+                  }`}
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+                {errors?.email && (
+                  <span
+                    style={{ color: "red" }}
+                    className={styles.errorMessage}
+                  >
+                    Please enter a valid email address.
+                  </span>
+                )}
+              </div>
+            )}
+            <input
+              style={{ marginTop: "5px" }}
+              type="text"
+              placeholder="Full Name"
+              className={`${styles.input} ${
+                errors?.name ? styles.inputError : ""
+              }`}
+              value={name}
+              onChange={handleNameChange}
+            />
+            {errors?.name && (
               <span style={{ color: "red" }} className={styles.errorMessage}>
-                Please enter a valid email address.
+                Full name is required.
               </span>
             )}
+
+            <p className={styles.subText}>
+              We only use this to match you with trusted professionals.
+            </p>
           </div>
         )}
-        <input
-          style={{ marginTop: "5px" }}
-          type="text"
-          placeholder="Full Name"
-          className={`${styles.input} ${errors?.name ? styles.inputError : ""}`}
-          value={name}
-          onChange={handleNameChange}
-        />
-        {errors?.name && (
-          <span style={{ color: "red" }} className={styles.errorMessage}>
-            Full name is required.
-          </span>
-        )}
-
-        <p className={styles.subText}>
-          We only use this to match you with trusted professionals.
-        </p>
-      </div>
-    </CardLayoutWrapper>
+      </CardLayoutWrapper>
+    </BackgroundWrapperNameEmailMultiForm>
   );
 };
 
