dcc6e949cb784ec489dac107d2f1bb3c38697aa2
Deepak Birla <deepak@zuzucodes.com>
2025-11-13 16:58:14 +0530

    add-one-more-option-30-miles-in-seller-registration

--- src/component/servicePanel/FindLocalJobs/ServiceCreateAccount/OtherServiceStep/OtherServiceStep.jsx
+++ src/component/servicePanel/FindLocalJobs/ServiceCreateAccount/OtherServiceStep/OtherServiceStep.jsx
@@ -435,6 +435,7 @@ const OtherServiceStep = ({
                 <option value="5">5 miles</option>
                 <option value="10">10 miles</option>
                 <option value="20">20 miles</option>
+                <option value="30">30 miles</option>
                 <option value="50">50 miles</option>
                 <option value="100">100 miles</option>
               </select>
--- src/component/servicePanel/FindLocalJobs/ServiceCreateAccount/ServiceLocationStep/ServiceLocationStep.jsx
+++ src/component/servicePanel/FindLocalJobs/ServiceCreateAccount/ServiceLocationStep/ServiceLocationStep.jsx
@@ -144,6 +144,7 @@ const ServiceLocationStep = ({
                 <option value="5">5 miles</option>
                 <option value="10">10 miles</option>
                 <option value="20">20 miles</option>
+                 <option value="30">30 miles</option>
                 <option value="50">50 miles</option>
                 <option value="100">100 miles</option>
               </select>
