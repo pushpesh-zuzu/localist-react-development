e1ac6cf5e7ec93b792ada0ffc24a696bcb5f4691
Deepak Birla <deepak@zuzucodes.com>
2025-11-17 22:18:02 +0530

    fixed-roofing-text

--- src/component/MultiStepForm/steps/QuestionAnswerMultiStep/QuestionAnserMultiStepRoofingNew2.jsx
+++ src/component/MultiStepForm/steps/QuestionAnswerMultiStep/QuestionAnserMultiStepRoofingNew2.jsx
@@ -359,7 +359,7 @@ const QuestionAnserMultiStepRoofingNew2 = ({
       subtitle={
         currentQuestion === 0
           ? !isQuestionWithImage
-            ? "To find the ideal driveway installers specialist for your project, simply complete the quick form below."
+            ? "To find the ideal roofing companies for your project, simply complete the quick form below."
             : ""
           : ""
       }
