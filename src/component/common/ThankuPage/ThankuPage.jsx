19f58ab8fdfc9c8df4b8c21547302255cc3894f2
Deepak Birla <deepakbirla@zuzucodes.com>
2025-11-26 13:54:27 +0530

    fix-add-new-script-head-and-add-login-correct-route-in-protect-route

--- src/component/common/ThankuPage/ThankuPage.jsx
+++ src/component/common/ThankuPage/ThankuPage.jsx
@@ -36,10 +36,39 @@ const ThankuPage = () => {
             });
           `}
         </script>
-        {/* Bing UET Tracking */}
-        <script>{`
-          (function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"97207664", enableAutoSpaTracking: true};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");
-        `}</script>
+        <script>
+          {`(function (w, d, t, r, u) {
+        var f, n, i;
+        (w[u] = w[u] || []),
+          (f = function () {
+            var o = { ti: "97207664", enableAutoSpaTracking: true };
+            (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
+          }),
+          (n = d.createElement(t)),
+          (n.src = r),
+          (n.async = 1),
+          (n.onload = n.onreadystatechange =
+            function () {
+              var s = this.readyState;
+              (s && s !== "loaded" && s !== "complete") ||
+                (f(), (n.onload = n.onreadystatechange = null));
+            }),
+          (i = d.getElementsByTagName(t)[0]),
+          i.parentNode.insertBefore(n, i);
+      })(window, document, "script", "//bat.bing.com/bat.js", "uetq");`}
+        </script>
+        <script>
+          {` window.uetq = window.uetq || [];
+      window.uetq.push("consent", "default", {
+        ad_storage: "denied",
+      });
+    </script>
+    <script>
+      window.uetq = window.uetq || [];
+      window.uetq.push("consent", "update", {
+        ad_storage: "granted",
+      });`}
+        </script>
       </Helmet>
       <div className={styles.pageWrapper}>
         <div className={styles.modalContent}>
